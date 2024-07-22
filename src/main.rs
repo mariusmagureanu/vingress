04b0820fc9055f8474861f693d47f72955b6f145
Marius Magureanu <mariusmagureanu@live.dk>
2024-07-22 09:22:14 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use futures::TryStreamExt;
+use k8s_openapi::api::networking::v1::Ingress;
+use kube::{
+    runtime::{watcher, WatchStreamExt},
+    Api, Client,
+};
+use std::pin::pin;
+
+#[tokio::main]
+async fn main() {
+    let client = Client::try_default().await.unwrap();
+
+    let ings: Api<Ingress> = Api::all(client);
+
+    let wc = watcher::Config::default();
+    let obs = watcher(ings, wc).default_backoff().applied_objects();
+
+    let mut obs = pin!(obs);
+
+    while let Some(n) = obs.try_next().await.unwrap() {
+        println!("{:?}", n);
+    }
+}
