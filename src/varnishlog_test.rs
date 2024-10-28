185d4d367ebfd6bf20dcd1ececb3b2cc3978c0ff
Marius Magureanu <mariusmagureanu@live.dk>
2024-10-28 15:28:36 +0100

    Added a new test

--- src/varnishlog_test.rs
+++ src/varnishlog_test.rs
@@ -72,110 +72,89 @@ mod test {
         assert_eq!(state.resp_headers, expected_request.resp_headers);
     }
 
-    // #[test]
-    // fn test_parse_backend_request() {
-    //     let log = r#"
-    //         **  << BeReq    >> 24169
-    //         --  Begin          bereq 24168 fetch
-    //         --  VCL_use        reload_20241006_164229_237
-    //         --  Timestamp      Start: 1728643157.173163 0.000000 0.000000
-    //         --  BereqMethod    GET
-    //         --  BereqURL       /foo
-    //         --  BereqProtocol  HTTP/1.1
-    //         --  BereqHeader    Host: foo.bar.com
-    //         --  BereqHeader    User-Agent: curl/8.10.1
-    //         --  BereqHeader    Accept: */*
-    //         --  BereqHeader    X-Forwarded-For: 10.125.174.2
-    //         --  BereqHeader    Via: 1.1 varnish-ingress-controller-d465bf5c5-7pd9l (Varnish/7.6)
-    //         --  BereqHeader    Accept-Encoding: gzip
-    //         --  BereqHeader    X-Varnish: 24169
-    //         --  VCL_call       BACKEND_FETCH
-    //         --  VCL_return     fetch
-    //         --  Timestamp      Fetch: 1728643157.173180 0.000017 0.000017
-    //         --  Timestamp      Connected: 1728643157.173341 0.000178 0.000160
-    //         --  BackendOpen    22 demo-media-media-v1-svc 172.20.163.200 80 10.125.161.62 57622 connect
-    //         --  Timestamp      Bereq: 1728643157.173377 0.000214 0.000036
-    //         --  BerespProtocol HTTP/1.1
-    //         --  BerespStatus   200
-    //         --  BerespReason   OK
-    //         --  BerespHeader   Server: nginx/1.27.1
-    //         --  BerespHeader   Date: Fri, 11 Oct 2024 10:39:17 GMT
-    //         --  BerespHeader   Content-Type: text/plain
-    //         --  BerespHeader   Content-Length: 162
-    //         --  BerespHeader   Connection: keep-alive
-    //         --  BerespHeader   Expires: Fri, 11 Oct 2024 10:39:16 GMT
-    //         --  BerespHeader   Cache-Control: no-cache
-    //         --  Timestamp      Beresp: 1728643157.173478 0.000315 0.000101
-    //         --  TTL            RFC 0 10 0 1728643157 1728643157 1728643157 1728643156 0 cacheable
-    //         --  VCL_call       BACKEND_RESPONSE
-    //         --  BerespUnset    Cache-Control: no-cache
-    //         --  VCL_return     deliver
-    //         --  Timestamp      Process: 1728643157.173570 0.000406 0.000091
-    //         --  Storage        malloc s0
-    //         --  Fetch_Body     3 length stream
-    //         --  BackendClose   22 demo-media-media-v1-svc recycle
-    //         --  Timestamp      BerespBody: 1728643157.173638 0.000475 0.000068
-    //         --  Length         162
-    //         --  BereqAcct      217 0 217 214 162 376
-    //         --  End
-    //     "#;
-    //
-    //     let backend_request = parse_backend_request(log);
-    //
-    //     // Assertions
-    //     assert_eq!(backend_request.id, 24169);
-    //     assert_eq!(backend_request.bereq_id, 24168);
-    //     assert_eq!(backend_request.vcl_use, "reload_20241006_164229_237");
-    //     assert_eq!(backend_request.bereq_method, "GET");
-    //     assert_eq!(backend_request.bereq_url, "/foo");
-    //     assert_eq!(backend_request.bereq_protocol, "HTTP/1.1");
-    //
-    //     // Checking some headers
-    //     assert_eq!(
-    //         backend_request.bereq_headers[0],
-    //         ("Host".to_string(), "foo.bar.com".to_string())
-    //     );
-    //     assert_eq!(
-    //         backend_request.bereq_headers[1],
-    //         ("User-Agent".to_string(), "curl/8.10.1".to_string())
-    //     );
-    //
-    //     // Check backend open details
-    //     assert_eq!(backend_request.backend_open.conn_id, 22);
-    //     assert_eq!(
-    //         backend_request.backend_open.backend_name,
-    //         "demo-media-media-v1-svc"
-    //     );
-    //     assert_eq!(backend_request.backend_open.backend_ip, "172.20.163.200");
-    //     assert_eq!(backend_request.backend_open.backend_port, 80);
-    //     assert_eq!(backend_request.backend_open.client_ip, "10.125.161.62");
-    //     assert_eq!(backend_request.backend_open.client_port, 57622);
-    //     assert_eq!(backend_request.backend_open.connect_action, "connect");
-    //
-    //     // Check Beresp status
-    //     assert_eq!(backend_request.beresp_protocol, "HTTP/1.1");
-    //     assert_eq!(backend_request.beresp_status, 200);
-    //     assert_eq!(backend_request.beresp_reason, "OK");
-    //
-    //     // Check TTL
-    //     assert_eq!(backend_request.ttl.ttl_type, "RFC");
-    //     assert_eq!(backend_request.ttl.ttl_value, 0);
-    //     assert_eq!(backend_request.ttl.grace, 10);
-    //     assert_eq!(backend_request.ttl.keep, 0);
-    //     assert!(backend_request.ttl.cacheable);
-    //
-    //     // Check storage
-    //     assert_eq!(backend_request.storage, "malloc");
-    //
-    //     // Check length
-    //     assert_eq!(backend_request.length, 162);
-    //
-    //     // Check BereqAcct details
-    //     assert_eq!(backend_request.bereq_acct.req_header_bytes, 217);
-    //     assert_eq!(backend_request.bereq_acct.req_body_bytes, 0);
-    //     assert_eq!(backend_request.bereq_acct.total_req_bytes, 217);
-    //     assert_eq!(backend_request.bereq_acct.resp_header_bytes, 214);
-    //     assert_eq!(backend_request.bereq_acct.resp_body_bytes, 162);
-    //     assert_eq!(backend_request.bereq_acct.total_resp_bytes, 376);
-    // }
+    #[tokio::test]
+    async fn test_parse_backend_request() {
+        let input_lines = r#"**  << BeReq    >> 24169
+            --  Begin          bereq 24168 fetch
+            --  VCL_use        reload_20241006_164229_237
+            --  Timestamp      Start: 1728643157.173163 0.000000 0.000000
+            --  BereqMethod    GET
+            --  BereqURL       /foo
+            --  BereqProtocol  HTTP/1.1
+            --  BereqHeader    Host: foo.bar.com
+            --  BereqHeader    User-Agent: curl/8.10.1
+            --  BereqHeader    Accept: */*
+            --  BereqHeader    X-Forwarded-For: 10.125.174.2
+            --  BereqHeader    Via: 1.1 varnish-ingress-controller-d465bf5c5-7pd9l (Varnish/7.6)
+            --  BereqHeader    Accept-Encoding: gzip
+            --  BereqHeader    X-Varnish: 24169
+            --  VCL_call       BACKEND_FETCH
+            --  VCL_return     fetch
+            --  Timestamp      Fetch: 1728643157.173180 0.000017 0.000017
+            --  Timestamp      Connected: 1728643157.173341 0.000178 0.000160
+            --  BackendOpen    22 demo-media-media-v1-svc 172.20.163.200 80 10.125.161.62 57622 connect
+            --  Timestamp      Bereq: 1728643157.173377 0.000214 0.000036
+            --  BerespProtocol HTTP/1.1
+            --  BerespStatus   200
+            --  BerespReason   OK
+            --  BerespHeader   Server: nginx/1.27.1
+            --  BerespHeader   Date: Fri, 11 Oct 2024 10:39:17 GMT
+            --  BerespHeader   Content-Type: text/plain
+            --  BerespHeader   Content-Length: 162
+            --  BerespHeader   Connection: keep-alive
+            --  BerespHeader   Expires: Fri, 11 Oct 2024 10:39:16 GMT
+            --  BerespHeader   Cache-Control: no-cache
+            --  Timestamp      Beresp: 1728643157.173478 0.000315 0.000101
+            --  TTL            RFC 0 10 0 1728643157 1728643157 1728643157 1728643156 0 cacheable
+            --  VCL_call       BACKEND_RESPONSE
+            --  BerespUnset    Cache-Control: no-cache
+            --  VCL_return     deliver
+            --  Timestamp      Process: 1728643157.173570 0.000406 0.000091
+            --  Storage        malloc s0
+            --  Fetch_Body     3 length stream
+            --  BackendClose   22 demo-media-media-v1-svc recycle
+            --  Timestamp      BerespBody: 1728643157.173638 0.000475 0.000068
+            --  Length         162
+            --  BereqAcct      217 0 217 214 162 376
+            --  End            "#;
+
+        let re_patterns = get_regex_patterns();
+        let mut state = RequestState::default();
+
+        for line in input_lines.lines() {
+            let line = line.trim();
+            parse_log_line(&line, &re_patterns, &mut state).await;
+        }
+
+        let expected_request = RequestState {
+            method: "".to_string(),
+            protocol: "".to_string(),
+            resp_status: "".to_string(),
+            resp_reason: "".to_string(),
+            resp_headers: vec![],
+            req_headers: vec![],
+            url: "/foo".to_string(),
+            beresp_status: "200".to_string(),
+            beresp_reason: "OK".to_string(),
+            beresp_headers: vec![
+                ("Server".to_string(), "nginx/1.27.1".to_string()),
+                (
+                    "Date".to_string(),
+                    "Fri, 11 Oct 2024 10:39:17 GMT".to_string(),
+                ),
+                ("Content-Type".to_string(), "text/plain".to_string()),
+                ("Content-Length".to_string(), "162".to_string()),
+                ("Connection".to_string(), "keep-alive".to_string()),
+                (
+                    "Expires".to_string(),
+                    "Fri, 11 Oct 2024 10:39:16 GMT".to_string(),
+                ),
+                ("Cache-Control".to_string(), "no-cache".to_string()),
+            ],
+        };
+
+        assert_eq!(state.beresp_reason, expected_request.beresp_reason);
+        assert_eq!(state.beresp_status, expected_request.beresp_status);
+        assert_eq!(state.beresp_headers, expected_request.beresp_headers);
+    }
 }
