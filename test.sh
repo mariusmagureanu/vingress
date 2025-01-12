d1089ea1a1b563b116e47b104c377188a28f55b9
Marius Magureanu <mariusmagureanu@live.dk>
2025-01-12 21:16:38 +0100

    Pick up bash from env

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/usr/bin/env sh
 kubectl config use-context kind-varnish
 docker build -t vingress-dev:latest . -f Dockerfile.dev
 kind load docker-image vingress-dev:latest -n varnish
